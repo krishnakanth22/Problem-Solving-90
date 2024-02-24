559f88b0d027d8353f5184613041b65ccb285fed
KRISHNA KANTH <117899985+krishnakanth22@users.noreply.github.com>
2024-02-24 19:26:20 +0530

    Create Find_all_people_secert.py
    You are given an integer n indicating there are n people numbered from 0 to n - 1. You are also given a 0-indexed 2D integer array meetings where meetings[i] = [xi, yi, timei] indicates that person xi and person yi have a meeting at timei. A person may attend multiple meetings at the same time. Finally, you are given an integer firstPerson.
    
    Person 0 has a secret and initially shares the secret with a person firstPerson at time 0. This secret is then shared every time a meeting takes place with a person that has the secret. More formally, for every meeting, if a person xi has the secret at timei, then they will share the secret with person yi, and vice versa.
    
    The secrets are shared instantaneously. That is, a person may receive the secret and share it with people in other meetings within the same time frame.
    
    Return a list of all the people that have the secret after all the meetings have taken place. You may return the answer in any order.

---
+++ Find_all_people_secert.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        secrets = set([0, firstPerson]) 
+        time_map = {}
+
+        for src, dst, t in meetings:
+            if t not in time_map:
+                time_map[t] = defaultdict(list)
+            time_map[t][src].append(dst)
+            time_map[t][dst].append(src)
+
+        def dfs(src, adj):
+            if src in visit:
+                return
+            visit.add(src)
+            secrets.add(src)
+            for nei in adj[src]:
+                secrets.add(nei)  
+                dfs(nei, adj)
+
+        for t in sorted(time_map.keys()):
+            visit = set()
+            for src in time_map[t].keys():
+                if src in secrets:
+                    dfs(src, time_map[t])
+
+        return list(secrets)
