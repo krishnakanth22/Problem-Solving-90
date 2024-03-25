7d4bdce0cc4b983d3fec5d4bfb98eef4344871a5
KRISHNA KANTH <117899985+krishnakanth22@users.noreply.github.com>
2024-03-25 12:17:56 +0530

    Create Sum of Two Integers2.py
    Given two integers a and b, return the sum of the two integers without using the operators + and -.

---
+++ Sum of Two Integers2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        '''mask = 0xFFFFFFFF
+        while b != 0:
+            carry = (a & b) & mask
+            
+            a = (a ^ b) & mask
+            b = ((carry << 1) & mask) if carry != 0 else 0
+        return a if a <= 0x7FFFFFFF else ~(a ^ mask)'''
+        list=[a,b]
+        return sum(list)
