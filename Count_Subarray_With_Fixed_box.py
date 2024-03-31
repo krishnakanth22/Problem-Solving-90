665d08c3d6820786f49eff4cc6dac38e350ef013
KRISHNA KANTH <117899985+krishnakanth22@users.noreply.github.com>
2024-03-31 21:42:03 +0530

    Create Count_Subarray_With_Fixed_box.py
    You are given an integer array nums and two integers minK and maxK.
    
    A fixed-bound subarray of nums is a subarray that satisfies the following conditions:
    
    The minimum value in the subarray is equal to minK.
    The maximum value in the subarray is equal to maxK.
    Return the number of fixed-bound subarrays.
    
    A subarray is a contiguous part of an array.

---
+++ Count_Subarray_With_Fixed_box.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        res = 0
+        bad_idx = left_idx = right_idx = -1
+
+        for i, num in enumerate(nums):
+            if not minK <= num <= maxK:
+                bad_idx = i
+
+            if num == minK:
+                left_idx = i
+
+            if num == maxK:
+                right_idx = i
+
+            res += max(0, min(left_idx, right_idx) - bad_idx)
+
+        return res
