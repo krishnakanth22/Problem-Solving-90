04f4b2b04fede8099a20ab3b1ac36fc6560b0de3
KRISHNA KANTH <117899985+krishnakanth22@users.noreply.github.com>
2024-06-25 18:26:26 +0530

    Update Number_of_Island.py

--- Number_of_Island.py
+++ Number_of_Island.py
@@ -1,23 +1,23 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0':
-                return
-            grid[i][j] = '0'
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
-
-        if not grid:
-            return 0
-
-        num_islands = 0
-        rows, cols = len(grid), len(grid[0])
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == '1':
-                    num_islands += 1
-                    dfs(i, j)
-
-        return num_islands
+def numIslands(grid):
+    def dfs(i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0':
+            return
+        grid[i][j] = '0'
+        dfs(i+1, j)
+        dfs(i-1, j)
+        dfs(i, j+1)
+        dfs(i, j-1)
+    
+    if not grid or not grid[0]:
+        return 0
+    
+    num_islands = 0
+    rows, cols = len(grid), len(grid[0])
+    
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == '1': 
+                num_islands += 1
+                dfs(i, j) 
+    
+    return num_islands
