f8dcb738e1557f31ce61bd5b09212456b6fb82f1
KRISHNA KANTH <117899985+krishnakanth22@users.noreply.github.com>
2024-06-29 23:00:29 +0530

    Create BFS_Traversal.java
    Using the linkedList Data reading implement the Breadth first search  by imp[lemeting queue

---
+++ BFS_Traversal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Graph{
+    int vertices;
+    LinkedList<Integer>[] adjlist;
+    Graph(int vertices){
+        this.vertices=vertices;
+        adjlist=new LinkedList[vertices]
+        for vertices[i]=new 
+    }
+
+}
