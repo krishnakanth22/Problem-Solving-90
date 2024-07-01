2d19f7fb1746293ab909b13c84e7486f40606677
KRISHNA KANTH <117899985+krishnakanth22@users.noreply.github.com>
2024-07-01 20:07:19 +0530

    Create threeConsecutiveOdds.java

---
+++ threeConsecutiveOdds.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+    int odds = 0;
+    for (int i = 0; i < arr.length && odds < 3; i++) {
+        odds = (arr[i] % 2 == 1) ? odds + 1 : 0;
+    }
+    return odds == 3;
+    }   
+}
